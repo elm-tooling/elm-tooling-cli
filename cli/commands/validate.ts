f1e167a6758f342f821ab2bab3904de4969e242f
Simon Lydell <simon.lydell@gmail.com>
2020-09-02 21:13:37 +0200

    Validate that elm.json exists

--- cli/commands/validate.ts
+++ cli/commands/validate.ts
@@ -38,7 +38,16 @@ export default function validate(): number {
     return 1;
   }
 
-  const validationErrors = validateJson(elmToolingPath, json);
+  const elmJsonError = validateFileExists(
+    path.join(path.dirname(elmToolingPath), "elm.json")
+  );
+
+  const validationErrors = [
+    ...(elmJsonError === undefined
+      ? []
+      : [`Expected an elm.json next to elm-tooling.json. ${elmJsonError}`]),
+    ...validateJson(elmToolingPath, json),
+  ];
 
   if (validationErrors.length === 0) {
     console.error("No errors found.");
@@ -66,6 +75,21 @@ function validateJson(elmToolingPath: string, json: unknown): Array<string> {
   });
 }
 
+function validateFileExists(fullPath: string): string | undefined {
+  try {
+    const stats = fs.statSync(fullPath);
+    if (!stats.isFile()) {
+      return `Exists but is not a file: ${fullPath}`;
+    }
+  } catch (error) {
+    if ((error as Error & { code: string }).code === "ENOENT") {
+      return `File does not exist: ${fullPath}`;
+    }
+    return `File error for ${fullPath}: ${(error as Error).message}`;
+  }
+  return undefined;
+}
+
 function validateEntrypoints(
   elmToolingPath: string,
   value: unknown
@@ -78,36 +102,25 @@ function validateEntrypoints(
     return [`entrypoints: Expected at least one entrypoint but got 0`];
   }
 
-  return value.flatMap((entrypoint, index) => {
-    if (typeof entrypoint !== "string") {
-      return `entrypoints[${index}]: Expected a string but got: ${JSON.stringify(
-        entrypoint
-      )}`;
-    }
-
-    if (!entrypoint.startsWith("./")) {
-      return `entrypoints[${index}]: Expected the string to start with "./" (to indicate that it is a relative path) but got: ${JSON.stringify(
-        entrypoint
-      )}`;
-    }
-
-    const fullPath = path.join(path.dirname(elmToolingPath), entrypoint);
-    try {
-      const stats = fs.statSync(fullPath);
-      if (!stats.isFile()) {
-        return `entrypoints[${index}]: Exists but is not a file: ${fullPath}`;
+  return value
+    .flatMap((entrypoint) => {
+      if (typeof entrypoint !== "string") {
+        return `Expected a string but got: ${JSON.stringify(entrypoint)}`;
       }
-    } catch (error) {
-      if ((error as Error & { code: string }).code === "ENOENT") {
-        return `entrypoints[${index}]: File does not exist: ${fullPath}`;
-      }
-      return `entrypoints[${index}]: File error for ${fullPath}: ${
-        (error as Error).message
-      }`;
-    }
 
-    return [];
-  });
+      if (!entrypoint.startsWith("./")) {
+        return `Expected the string to start with "./" (to indicate that it is a relative path) but got: ${JSON.stringify(
+          entrypoint
+        )}`;
+      }
+
+      return (
+        validateFileExists(
+          path.join(path.dirname(elmToolingPath), entrypoint)
+        ) ?? []
+      );
+    })
+    .map((error, index) => `entrypoints[${index}]: ${error}`);
 }
 
 function validateTools(_elmToolingPath: string, value: unknown): Array<string> {
@@ -116,20 +129,25 @@ function validateTools(_elmToolingPath: string, value: unknown): Array<string> {
   }
 
   return Object.entries(value).flatMap(([name, version]) => {
-    if (typeof version !== "string") {
-      return `tools[${JSON.stringify(
-        name
-      )}]: Expected a version as a string but got: ${JSON.stringify(version)}`;
-    }
-
-    const versions = Object.prototype.hasOwnProperty.call(tools, name)
-      ? tools[name]
-      : undefined;
-
-    return versions === undefined
-      ? `tools[${JSON.stringify(name)}]: Unknown tool`
-      : Object.prototype.hasOwnProperty.call(versions, version)
+    const error = validateTool(name, version);
+    return error === undefined
       ? []
-      : `tools[${JSON.stringify(name)}]: Unknown version: ${version}`;
+      : `tools[${JSON.stringify(name)}]: ${error}`;
   });
+}
+
+function validateTool(name: string, version: unknown): string | undefined {
+  if (typeof version !== "string") {
+    return `Expected a version as a string but got: ${JSON.stringify(version)}`;
+  }
+
+  const versions = Object.prototype.hasOwnProperty.call(tools, name)
+    ? tools[name]
+    : undefined;
+
+  return versions === undefined
+    ? `Unknown tool`
+    : Object.prototype.hasOwnProperty.call(versions, version)
+    ? undefined
+    : `Unknown version: ${version}`;
 }
