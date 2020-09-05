77c81dce0da11ee1c6d7b9bd597cdb14ac815912
Simon Lydell <simon.lydell@gmail.com>
2020-09-05 02:49:37 +0200

    Working download

--- cli/commands/download.ts
+++ cli/commands/download.ts
@@ -1,7 +1,15 @@
+import * as childProcess from "child_process";
+import * as crypto from "crypto";
 import * as fs from "fs";
+import * as https from "https";
+import * as os from "os";
+import * as path from "path";
+import * as readline from "readline";
+import type { Writable } from "stream";
+import * as zlib from "zlib";
 
 import { findClosestElmTooling, isRecord } from "../helpers/mixed";
-import { Asset, OSName, tools } from "../helpers/tools";
+import { Asset, AssetType, OSName, tools } from "../helpers/tools";
 
 export default async function download(): Promise<number> {
   const osName = getOSName();
@@ -64,13 +72,52 @@ export default async function download(): Promise<number> {
     return 0;
   }
 
-  console.error(`Downloading:`);
-  console.error(
-    assets
-      .map(({ name, version, asset }) => `${name} ${version} (${asset.url})`)
-      .join("\n")
+  const progresses = assets.map(() => 0);
+  const firstDrawTime = Date.now();
+
+  const draw = () => {
+    console.error(
+      assets
+        .map(
+          ({ name, version }, index) =>
+            `${Math.round(progresses[index] * 100)
+              .toString()
+              .padStart(3)}% ${name} ${version}`
+        )
+        .join("\n")
+    );
+  };
+
+  const redraw = ({ force = false } = {}) => {
+    // Without this time thing, you’ll see the progress go up to 100% and then
+    // back to 0% again when using curl. It reports the progress per request –
+    // even redirects.
+    if (Date.now() - firstDrawTime > 1000 || force) {
+      readline.moveCursor(process.stderr, 0, -assets.length);
+      draw();
+    }
+  };
+
+  draw();
+
+  const [results, downloadErrors] = partition(
+    await Promise.all(
+      assets.map(({ name, version, asset }, index) =>
+        downloadAndExtract({ name, version, asset }, (percentage) => {
+          progresses[index] = percentage;
+          redraw();
+        }).catch(
+          (error: Error) => new Error(`${name} ${version}: ${error.message}`)
+        )
+      )
+    )
   );
-  return 0;
+
+  redraw({ force: true });
+  console.error(results);
+  console.error(downloadErrors);
+
+  return downloadErrors.length === 0 ? 0 : 1;
 }
 
 type NamedAsset = {
@@ -135,4 +182,285 @@ function partition<T>(items: Array<T | string>): [Array<string>, Array<T>] {
   }
 
   return [errors, results];
+}
+
+function downloadAndExtract(
+  { name, version, asset }: NamedAsset,
+  onProgress: (percentage: number) => void
+): Promise<string> {
+  return new Promise((resolve, reject): void => {
+    const elmHome = process.env.ELM_HOME || path.join(os.homedir(), ".elm");
+    const dir = path.join(elmHome, "elm-tooling", name, version);
+    const binary = path.join(dir, name);
+
+    if (fs.existsSync(binary)) {
+      resolve(`Already exists: ${binary}`);
+      return;
+    }
+
+    fs.mkdirSync(dir, { recursive: true });
+
+    const removeExtractedBeforeReject = (error: Error): void => {
+      try {
+        fs.unlinkSync(binary);
+        reject(error);
+      } catch (removeErrorAny) {
+        const removeError = removeErrorAny as Error & { code?: string };
+        if (removeError.code === "ENOENT") {
+          reject(error);
+        } else {
+          reject(new Error(`${error.message}\n${removeError.message}`));
+        }
+      }
+    };
+
+    const hash = crypto.createHash("sha256");
+
+    const extract = extractFile({
+      assetType: asset.type,
+      dir,
+      name,
+      onError: reject,
+      onSuccess: () => {
+        resolve("TODO all good");
+      },
+    });
+
+    // Allow `onError` for `extract` to fire before starting to download.
+    process.nextTick(() => {
+      downloadFile(asset.url, {
+        onData: (chunk) => {
+          hash.update(chunk);
+          extract.write(chunk);
+        },
+        onProgress,
+        onError: removeExtractedBeforeReject,
+        onSuccess: () => {
+          const digest = hash.digest("hex");
+          if (digest === asset.hash) {
+            extract.end();
+          } else {
+            removeExtractedBeforeReject(
+              new Error(hashMismatch(digest, asset.hash))
+            );
+          }
+        },
+      });
+    });
+  });
+}
+
+function hashMismatch(actual: string, expected: string) {
+  return `
+Hash mismatch:
+Expected: ${expected}
+Actual:   ${actual}
+  `.trim();
+}
+
+function downloadFile(
+  url: string,
+  {
+    onData,
+    onProgress,
+    onError,
+    onSuccess,
+  }: {
+    onData: (buffer: Buffer) => void;
+    onProgress: (percentage: number) => void;
+    onError: (error: Error & { code?: string }) => void;
+    onSuccess: () => void;
+  }
+): void {
+  let stderr = "";
+  const errored: Array<string> = [];
+
+  const onStderr = (chunk: Buffer): void => {
+    stderr += chunk.toString();
+    const matches = stderr.match(/\d+(?:\.\d+)?%/g) || [];
+    if (matches.length > 0) {
+      onProgress(Math.min(1, parseFloat(matches[matches.length - 1]) / 100));
+    }
+  };
+
+  const onClose = (commandName: string) => (code: number): void => {
+    if (errored.includes(commandName)) {
+      return;
+    } else if (code === 0) {
+      fs.writeFileSync("stderr.txt", stderr);
+      onSuccess();
+    } else {
+      onError(
+        new Error(
+          `${commandName} exited with non-zero exit code ${code}.\n${stderr}`
+        )
+      );
+    }
+  };
+
+  const curl = childProcess.spawn("curl", ["-#fL", url]);
+  curl.stdout.on("data", onData);
+  curl.stderr.on("data", onStderr);
+  curl.on("close", onClose("curl"));
+
+  curl.on("error", (error: Error & { code?: string }) => {
+    errored.push("curl");
+    if (error.code === "ENOENT") {
+      const wget = childProcess.spawn("wget", ["-O", "-", url]);
+      wget.stdout.on("data", onData);
+      wget.stderr.on("data", onStderr);
+      wget.on("close", onClose("wget"));
+
+      wget.on("error", (error: Error & { code?: string }) => {
+        errored.push("wget");
+        if (error.code === "ENOENT") {
+          downloadFileNative(url, {
+            onData,
+            onProgress,
+            onError,
+            onSuccess,
+          });
+        } else {
+          onError(error);
+        }
+      });
+    } else {
+      onError(error);
+    }
+  });
+}
+
+function downloadFileNative(
+  url: string,
+  {
+    onData,
+    onProgress,
+    onError,
+    onSuccess,
+  }: {
+    onData: (buffer: Buffer) => void;
+    onProgress: (percentage: number) => void;
+    onError: (error: Error & { code?: string }) => void;
+    onSuccess: () => void;
+  },
+  maxRedirects = 50 // This is curl’s default.
+) {
+  const request = https.get(url, (response) => {
+    switch (response.statusCode) {
+      case 302: {
+        const redirect = response.headers.location;
+        if (redirect === undefined) {
+          onError(new Error(`Got 302 without location header.`));
+        } else if (maxRedirects <= 0) {
+          onError(new Error(`Too many redirects.`));
+        } else {
+          downloadFileNative(
+            redirect,
+            {
+              onData,
+              onProgress,
+              onError,
+              onSuccess,
+            },
+            maxRedirects - 1
+          );
+        }
+        break;
+      }
+
+      case 200: {
+        const contentLength = parseInt(
+          response.headers["content-length"] || "",
+          10
+        );
+        let length = 0;
+
+        response.on("data", (chunk: Buffer) => {
+          length += chunk.length;
+          onData(chunk);
+          if (Number.isFinite(contentLength) && contentLength > 0) {
+            onProgress(Math.min(1, length / contentLength));
+          }
+        });
+
+        response.on("end", onSuccess);
+
+        break;
+      }
+
+      default:
+        onError(
+          new Error(
+            `Unexpected status code: ${response.statusCode ?? "unknown"}`
+          )
+        );
+        break;
+    }
+  });
+
+  request.on("error", onError);
+}
+
+function extractFile({
+  assetType,
+  dir,
+  name,
+  onError,
+  onSuccess,
+}: {
+  assetType: AssetType;
+  dir: string;
+  name: string;
+  onError: (error: Error) => void;
+  onSuccess: () => void;
+}): Writable {
+  switch (assetType) {
+    case "gz": {
+      const gunzip = zlib.createGunzip();
+      const write = fs.createWriteStream(path.join(dir, name));
+      gunzip.on("error", onError);
+      write.on("error", onError);
+      write.on("close", () => {
+        try {
+          fs.chmodSync(path.join(dir, name), "755");
+        } catch (error) {
+          onError(error);
+        }
+      });
+      gunzip.pipe(write);
+      return gunzip;
+    }
+
+    case "tgz": {
+      const tar = childProcess.spawn("tar", ["xf", "-", "-C", dir, name]);
+      let tarStderr = "";
+
+      tar.on("error", (error: Error & { code?: string }) => {
+        if (error.code === "ENOENT") {
+          onError(
+            new Error(`'tar' must be installed globally.\n${error.message}`)
+          );
+        }
+        onError(error);
+      });
+
+      tar.stderr.on("data", (chunk: Buffer) => {
+        tarStderr += chunk.toString();
+      });
+
+      tar.on("close", (code) => {
+        if (code === 0) {
+          onSuccess();
+        } else {
+          onError(
+            new Error(
+              `tar exited with non-zero exit code ${code}:\n${tarStderr}`
+            )
+          );
+        }
+      });
+
+      return tar.stdin;
+    }
+  }
 }
--- cli/helpers/tools.ts
+++ cli/helpers/tools.ts
@@ -13,7 +13,10 @@ export type OSName = keyof OS;
 export type Asset = {
   hash: string;
   url: string;
+  type: AssetType;
 };
+
+export type AssetType = "gz" | "tgz";
 
 export const tools: Tools = {
   elm: {
@@ -23,16 +26,19 @@ export const tools: Tools = {
           "d359adbee89823c641cda326938708d7227dc79aa1f162e0d8fe275f182f528a",
         url:
           "https://github.com/elm/compiler/releases/download/0.19.0/binary-for-linux-64-bit.gz",
+        type: "gz",
       },
       mac: {
         hash:
           "f1fa4dd9021e94c5a58b2be8843e3329095232ee3bd21a23524721a40eaabd35",
         url:
           "https://github.com/elm/compiler/releases/download/0.19.0/binary-for-mac-64-bit.gz",
+        type: "gz",
       },
       windows: {
         hash: "TODO",
         url: "TODO",
+        type: "gz",
       },
     },
     "0.19.1": {
@@ -41,16 +47,19 @@ export const tools: Tools = {
           "e44af52bb27f725a973478e589d990a6428e115fe1bb14f03833134d6c0f155c",
         url:
           "https://github.com/elm/compiler/releases/download/0.19.1/binary-for-linux-64-bit.gz",
+        type: "gz",
       },
       mac: {
         hash:
           "05289f0e3d4f30033487c05e689964c3bb17c0c48012510dbef1df43868545d1",
         url:
           "https://github.com/elm/compiler/releases/download/0.19.1/binary-for-mac-64-bit.gz",
+        type: "gz",
       },
       windows: {
         hash: "TODO",
         url: "TODO",
+        type: "gz",
       },
     },
   },
@@ -61,16 +70,19 @@ export const tools: Tools = {
           "13d06e0c3f3a9ef585c828ac5761ead148ea2f203573309306393e2d8066e1fd",
         url:
           "https://github.com/avh4/elm-format/releases/download/0.8.1/elm-format-0.8.1-linux-x64.tgz",
+        type: "tgz",
       },
       mac: {
         hash:
           "e1beba5d3090968cbbd879384617506f4c71a3ea3b01ce94d298e4893e82a640",
         url:
           "https://github.com/avh4/elm-format/releases/download/0.8.1/elm-format-0.8.1-mac-x64.tgz",
+        type: "tgz",
       },
       windows: {
         hash: "TODO",
         url: "TODO",
+        type: "tgz",
       },
     },
     "0.8.2": {
@@ -79,16 +91,19 @@ export const tools: Tools = {
           "a69a4d3c49ccb0dffb3067b35464dc492563274e5778c40625220f9f6b3fd06d",
         url:
           "https://github.com/avh4/elm-format/releases/download/0.8.2/elm-format-0.8.2-linux-x64.tgz",
+        type: "tgz",
       },
       mac: {
         hash:
           "1f6cc8663922e546645c0536fc9bf7a49351d0b2963d26fc8fcb43e5bc92d733",
         url:
           "https://github.com/avh4/elm-format/releases/download/0.8.2/elm-format-0.8.2-mac-x64.tgz",
+        type: "tgz",
       },
       windows: {
         hash: "TODO",
         url: "TODO",
+        type: "tgz",
       },
     },
     "0.8.3": {
@@ -97,16 +112,19 @@ export const tools: Tools = {
           "9012f3a372488d4a118dc5f8ff57cc61cd1753d7d878b393fa7f60d496e37084",
         url:
           "https://github.com/avh4/elm-format/releases/download/0.8.3/elm-format-0.8.3-linux-x64.tgz",
+        type: "tgz",
       },
       mac: {
         hash:
           "66c9d4c2fcc7e435726f25ca44509cdf2caff5000dd215b5a086db514576efc7",
         url:
           "https://github.com/avh4/elm-format/releases/download/0.8.3/elm-format-0.8.3-mac-x64.tgz",
+        type: "tgz",
       },
       windows: {
         hash: "TODO",
         url: "TODO",
+        type: "tgz",
       },
     },
   },
