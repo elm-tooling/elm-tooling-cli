4288dd020c8ad7b763a94473f1d6bd7a35258314
Simon Lydell <simon.lydell@gmail.com>
2020-09-08 09:20:21 +0200

    WIP Windows symlink shims

--- cli/commands/postinstall.ts
+++ cli/commands/postinstall.ts
@@ -2,7 +2,7 @@ import * as fs from "fs";
 import * as path from "path";
 
 import { bold, dim, findClosest, NonEmptyArray } from "../helpers/mixed";
-import type { Tool } from "../helpers/parse";
+import { isWindows, Tool } from "../helpers/parse";
 import download from "./download";
 
 export default async function postinstall(): Promise<number> {
@@ -40,38 +40,123 @@ function linkTools(tools: NonEmptyArray<Tool>): number {
   for (const tool of tools) {
     const linkPath = path.join(nodeModulesBinPath, tool.name);
 
-    // Just like npm, just overwrite whatever links are already in
+    // Just like npm, these overwrite whatever links are already in
     // `node_modules/.bin/`. Most likely it’s either old links from for example
     // the `elm` npm package, or links from previous runs of this script.
-    try {
-      fs.unlinkSync(linkPath);
-    } catch (errorAny) {
-      const error = errorAny as Error & { code?: string };
-      if (error.code !== "ENOENT") {
-        console.error(
-          `Failed to remove old link for ${tool.name} at ${linkPath}:\n${error.message}`
-        );
-        return 1;
-      }
-    }
+    const linkPathPresentationString = isWindows
+      ? symlinkShimWindows(tool, linkPath)
+      : symlink(tool, linkPath);
 
-    try {
-      // This fails with EPERM on Windows without "junction".
-      fs.symlinkSync(tool.absolutePath, linkPath, "junction");
-    } catch (errorAny) {
-      const error = errorAny as Error & { code?: number };
-      console.error(
-        `Failed to create link for ${tool.name} at ${linkPath}:\n${error.message}`
-      );
+    if (linkPathPresentationString instanceof Error) {
+      console.error(linkPathPresentationString.message);
       return 1;
     }
 
+    const s = linkPath === linkPathPresentationString ? "" : "s";
     console.log(
-      `${bold(`${tool.name} ${tool.version}`)} link created: ${dim(
-        `${linkPath} -> ${tool.absolutePath}`
+      `${bold(`${tool.name} ${tool.version}`)} link${s} created: ${dim(
+        `${linkPathPresentationString} -> ${tool.absolutePath}`
       )}`
     );
   }
 
   return 0;
+}
+
+function symlink(tool: Tool, linkPath: string): string | Error {
+  try {
+    fs.unlinkSync(linkPath);
+  } catch (errorAny) {
+    const error = errorAny as Error & { code?: string };
+    if (error.code !== "ENOENT") {
+      return new Error(
+        `Failed to remove old link for ${tool.name} at ${linkPath}:\n${error.message}`
+      );
+    }
+  }
+
+  try {
+    fs.symlinkSync(tool.absolutePath, linkPath);
+  } catch (errorAny) {
+    const error = errorAny as Error & { code?: number };
+    return new Error(
+      `Failed to create link for ${tool.name} at ${linkPath}:\n${error.message}`
+    );
+  }
+
+  return linkPath;
+}
+
+// Inspired by: https://github.com/npm/cmd-shim/blob/92c0a16d3c8a8c53ade76b5eae8efae01dd7f41f/index.js
+function symlinkShimWindows(tool: Tool, linkPath: string): string | Error {
+  const shLinkPath = linkPath;
+  const cmdLinkPath = `${linkPath}.cmd`;
+  const ps1LinkPath = `${linkPath}.ps1`;
+  const linkPathPresentationString = `${linkPath}{,.cmd,.ps1}`;
+
+  const shScript = makeShScript(tool.absolutePath);
+  const cmdScript = makeCmdScript(tool.absolutePath);
+  const ps1Script = makePs1Script(tool.absolutePath);
+
+  try {
+    fs.unlinkSync(shLinkPath);
+    fs.unlinkSync(cmdLinkPath);
+    fs.unlinkSync(ps1LinkPath);
+  } catch (errorAny) {
+    const error = errorAny as Error & { code?: string };
+    if (error.code !== "ENOENT") {
+      return new Error(
+        `Failed to remove old links for ${tool.name} at ${linkPathPresentationString}:\n${error.message}`
+      );
+    }
+  }
+
+  try {
+    fs.writeFileSync(shLinkPath, shScript);
+    fs.chmodSync(shLinkPath, "755");
+    fs.writeFileSync(cmdLinkPath, cmdScript);
+    fs.chmodSync(cmdLinkPath, "755");
+    fs.writeFileSync(ps1LinkPath, ps1Script);
+    fs.chmodSync(ps1LinkPath, "755");
+  } catch (errorAny) {
+    const error = errorAny as Error & { code?: number };
+    return new Error(
+      `Failed to create links for ${tool.name} at ${linkPathPresentationString}:\n${error.message}`
+    );
+  }
+
+  return linkPathPresentationString;
+}
+
+// TODO: Need to convert path to unix style? cygpath?
+function makeShScript(toolAbsolutePath: string): string {
+  return lf(`
+#!/bin/sh
+'${toolAbsolutePath.replace(/'/g, "'\\''")}' "$@"
+`);
+}
+
+// Note: Paths on Windows cannot contain `"`.
+function makeCmdScript(toolAbsolutePath: string): string {
+  return crlf(`
+@ECHO off
+"${toolAbsolutePath}" %*
+`);
+}
+
+// TODO: Escaping? exit $LASTEXITCODE needed?
+function makePs1Script(toolAbsolutePath: string): string {
+  return lf(`
+#!/usr/bin/env pwsh
+& '${toolAbsolutePath.replace(/'/g, "''")}' $args
+exit $LASTEXITCODE
+`);
+}
+
+function lf(string: string): string {
+  return `${string.trim()}\n`;
+}
+
+function crlf(string: string): string {
+  return lf(string).replace(/\n/g, "\r\n");
 }
