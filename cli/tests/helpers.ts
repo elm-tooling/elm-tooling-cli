75035194e0d072b4d39ee001e5f260094446fca1
Simon Lydell <simon.lydell@gmail.com>
2020-09-12 17:40:19 +0200

    Try to fix tests on Windows

--- cli/tests/helpers.ts
+++ cli/tests/helpers.ts
@@ -1,3 +1,4 @@
+import * as os from "os";
 import * as path from "path";
 import * as stream from "stream";
 
@@ -37,13 +38,21 @@ export class MemoryWriteStream extends stream.Writable {
 
 export function clean(string: string): string {
   const { root } = path.parse(__dirname);
-  return (
-    string
-      .split(__dirname)
-      .join(path.join(root, "Users", "you", "project"))
-      // eslint-disable-next-line no-control-regex
-      .replace(/\x1B\[0?m/g, "⧘")
-      // eslint-disable-next-line no-control-regex
-      .replace(/\x1B\[\d+m/g, "⧙")
-  );
+
+  // Replace start of absolute paths with hardcoded stuff so the tests pass on
+  // more than one computer. Replace colors for snapshots.
+  const cleaned = string
+    .split(__dirname)
+    .join(path.join(root, "Users", "you", "project"))
+    // eslint-disable-next-line no-control-regex
+    .replace(/\x1B\[0?m/g, "⧘")
+    // eslint-disable-next-line no-control-regex
+    .replace(/\x1B\[\d+m/g, "⧙");
+
+  // Convert Windows-style paths to Unix-style paths so we can use the same snapshots.
+  return os.platform() === "win32"
+    ? cleaned.replace(/[A-Z]:((?:\\\w+)+)/g, (_, fullPath: string) =>
+        fullPath.replace(/\\/g, "/")
+      )
+    : cleaned;
 }
