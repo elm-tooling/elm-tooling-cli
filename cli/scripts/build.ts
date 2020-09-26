902663f76bbeb50b902aa0646fd1418fd155a360
Simon Lydell <simon.lydell@gmail.com>
2020-09-26 17:04:14 +0200

    Simplify build

--- cli/scripts/build.ts
+++ cli/scripts/build.ts
@@ -1,3 +1,4 @@
+import * as childProcess from "child_process";
 import * as fs from "fs";
 import * as path from "path";
 
@@ -57,3 +58,21 @@ for (const { src, dest = src, transformSrc, transformDest } of FILES_TO_COPY) {
     fs.copyFileSync(path.join(DIR, src), path.join(BUILD, dest));
   }
 }
+
+childProcess.spawnSync("npx", ["tsc"], { shell: true, stdio: "inherit" });
+
+function modifyFile(
+  file: string,
+  transform: (content: string) => string
+): void {
+  fs.writeFileSync(file, transform(fs.readFileSync(file, "utf8")));
+}
+
+function adjustDefaultExport(content: string): string {
+  return content.replace(/^exports.default =/m, "module.exports =");
+}
+
+modifyFile(path.join(BUILD, "index.js"), adjustDefaultExport);
+modifyFile(path.join(BUILD, "ensure.js"), adjustDefaultExport);
+
+fs.chmodSync(path.join(BUILD, "index.js"), "755");
