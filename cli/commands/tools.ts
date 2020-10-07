552811c427cedd8ef602b95cb36ba3bc26d9c4f9
Simon Lydell <simon.lydell@gmail.com>
2020-10-07 13:10:27 +0200

    WIP redraw

--- cli/commands/tools.ts
+++ cli/commands/tools.ts
@@ -64,6 +64,15 @@ export default async function toolsCommand(
   }
 }
 
+type ToolChoice = { name: string; version: string };
+
+type State = {
+  tools: Array<ToolChoice>;
+  cursorTool: ToolChoice;
+};
+
+type Cmd = "None" | "Exit" | "Save";
+
 async function start(
   handleColor: Logger["handleColor"],
   stdin: ReadStream,
@@ -74,48 +83,58 @@ async function start(
     stdin.setRawMode(true);
     stdin.resume();
 
-    const content = handleColor(draw(tools));
-    stdout.write(content);
+    let state: State = {
+      tools,
+      // TODO: Is it safe to take the first one, or do we have to sort first?
+      cursorTool: tools.length > 0 ? tools[0] : getDefaultCursorTool(),
+    };
 
-    // TODO: Should start at first x, or latest version of first tool.
-    // This is just the inital cursor position state.
-    readline.moveCursor(stdout, 3, -(content.split("\n").length - 2));
+    let cursor: { x: number; y: number } = { x: 0, y: 0 };
+
+    const redraw = (): void => {
+      readline.moveCursor(stdout, -cursor.x, -cursor.y);
+      const content = `\n${handleColor(draw(state.tools))}\n\n${JSON.stringify(
+        cursor
+      )}\n${JSON.stringify({
+        name: state.cursorTool.name,
+        version: state.cursorTool.version,
+      })}\n`;
+      stdout.write(content);
+
+      const y = getCursorLine(state.cursorTool);
+      cursor = { x: 3, y };
+
+      readline.moveCursor(
+        stdout,
+        cursor.x,
+        -(content.split("\n").length - 1 - cursor.y)
+      );
+    };
+
+    redraw();
 
     stdin.on("data", (buffer: Buffer) => {
-      const data = buffer.toString();
-      // TODO: Do this in a functional way. Return new state and redraw.
-      switch (data) {
-        case "\x03": // ctrl+c
+      const [nextState, cmd] = update(buffer.toString(), state);
+      state = nextState;
+      redraw();
+
+      switch (cmd) {
+        case "None":
+          break;
+        case "Exit":
+          // TODO: Exit
           resolve(0);
           break;
-
-        case "\x1b[A": // up
-        case "k":
-          readline.moveCursor(stdout, 0, -1);
-          break;
-
-        case "\x1b[B": // down
-        case "j":
-          readline.moveCursor(stdout, 0, 1);
-          break;
-
-        case "\r": // enter
+        case "Save":
+          // TODO: Save
           resolve(0);
-          break;
-
-        case " ": // space
-          stdout.write("x");
-          readline.moveCursor(stdout, -1, 0);
-          break;
-
-        default:
           break;
       }
     });
   });
 }
 
-function draw(tools: Array<Tool>): string {
+function draw(tools: Array<ToolChoice>): string {
   return Object.keys(KNOWN_TOOLS)
     .map((name) => {
       const versions = Object.keys(KNOWN_TOOLS[name]);
@@ -142,4 +161,93 @@ function draw(tools: Array<Tool>): string {
       ].join("\n")
     )
     .join("\n\n");
+}
+
+function getDefaultCursorTool(): ToolChoice {
+  const [name] = Object.keys(KNOWN_TOOLS);
+  const versions = Object.keys(KNOWN_TOOLS[name]);
+  const version = versions[versions.length - 1];
+  return { name, version };
+}
+
+function getCursorLine(cursorTool: ToolChoice): number {
+  const names = Object.keys(KNOWN_TOOLS);
+  const nameIndex = names.indexOf(cursorTool.name);
+  if (nameIndex === -1) {
+    return 1;
+  }
+  const name = names[nameIndex];
+  const versions = Object.keys(KNOWN_TOOLS[name]);
+  const versionIndex = versions.indexOf(cursorTool.version);
+  if (versionIndex === -1) {
+    return 1;
+  }
+  const previous = names
+    .slice(0, nameIndex)
+    .reduce(
+      (sum, name2) => sum + 2 + Object.keys(KNOWN_TOOLS[name2]).length,
+      0
+    );
+  return 1 + previous + versionIndex;
+}
+
+function update(keypress: string, state: State): [State, Cmd] {
+  switch (keypress) {
+    case "\x03": // ctrl+c
+      return [state, "Exit"];
+
+    case "\x1b[A": // up
+    case "k":
+      return [
+        { ...state, cursorTool: updateCursorTool(-1, state.cursorTool) },
+        "None",
+      ];
+
+    case "\x1b[B": // down
+    case "j":
+      return [
+        { ...state, cursorTool: updateCursorTool(1, state.cursorTool) },
+        "None",
+      ];
+
+    case "\r": // enter
+      return [state, "Save"];
+
+    case " ": // space
+      return [
+        { ...state, tools: toggleTool(state.cursorTool, state.tools) },
+        "None",
+      ];
+
+    default:
+      return [state, "None"];
+  }
+}
+
+function updateCursorTool(delta: number, cursorTool: ToolChoice): ToolChoice {
+  const all = Object.keys(KNOWN_TOOLS).flatMap((name) =>
+    Object.keys(KNOWN_TOOLS[name]).map((version) => ({ name, version }))
+  );
+  const index = all.findIndex(
+    (tool) =>
+      tool.name === cursorTool.name && tool.version === cursorTool.version
+  );
+  if (index === -1) {
+    return cursorTool;
+  }
+  const nextIndex = index + delta;
+  return nextIndex <= 0 || nextIndex >= all.length - 1
+    ? cursorTool
+    : all[nextIndex];
+}
+
+function toggleTool(
+  cursorTool: ToolChoice,
+  tools: Array<ToolChoice>
+): Array<ToolChoice> {
+  const filtered = tools.filter(
+    (tool) =>
+      !(tool.name === cursorTool.name && tool.version === cursorTool.version)
+  );
+  return filtered.length === tools.length ? [...tools, cursorTool] : filtered;
 }
