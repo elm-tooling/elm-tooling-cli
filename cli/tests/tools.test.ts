3f9494248e47f624d0d5fd242ba654786efac405
Simon Lydell <simon.lydell@gmail.com>
2020-10-08 19:17:05 +0200

    Fixtures

--- cli/tests/tools.test.ts
+++ cli/tests/tools.test.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+
 import elmToolingCli from "../index";
 import {
   clean,
@@ -7,16 +9,20 @@ import {
   stringSnapshotSerializer,
 } from "./helpers";
 
+const FIXTURES_DIR = path.join(__dirname, "fixtures", "tools");
+
 async function toolsHelper(
-  args: Array<string>,
+  fixture: string,
   chars: Array<string>
 ): Promise<string> {
+  const dir = path.join(FIXTURES_DIR, fixture);
+
   const stdout = new CursorWriteStream();
   const stderr = new MemoryWriteStream();
 
-  const exitCode = await elmToolingCli(args, {
-    cwd: __dirname,
-    env: {},
+  const exitCode = await elmToolingCli(["tools"], {
+    cwd: dir,
+    env: { ELM_HOME: dir },
     stdin: new RawReadStream(chars),
     stdout,
     stderr,
@@ -31,22 +37,75 @@ async function toolsHelper(
 expect.addSnapshotSerializer(stringSnapshotSerializer);
 
 describe("tools", () => {
-  test("first test", async () => {
-    expect(await toolsHelper(["tools"], ["q"])).toMatchInlineSnapshot(`
-      ⧙/Users/lydell/src/elm-tooling.json/cli/elm-tooling.json⧘
+  test("default cursor position when no tools", async () => {
+    expect(await toolsHelper("empty-elm-tooling", ["test-exit"]))
+      .toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/empty-elm-tooling/elm-tooling.json⧘
 
       ⧙elm⧘
         ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
-        ⧙[⧘⧙x⧘⧙]⧘ 0.19.1
+        ⧙[⧘▊⧙]⧘ ⧙0.19.1⧘
 
       ⧙elm-format⧘
         ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
         ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
         ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
-        ⧙[⧘⧙x⧘⧙]⧘ 0.8.4
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.2.8⧘
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+         
+    `);
+  });
+
+  test("default cursor position when tools are provided", async () => {
+    expect(await toolsHelper("some-elm-tooling", ["test-exit"]))
+      .toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/some-elm-tooling/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘⧙☒⧘⧙]⧘ 0.8.3
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
 
       ⧙elm-json⧘
         ⧙[⧘⧙x⧘⧙]⧘ 0.2.8
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+         
+    `);
+  });
+
+  test("moves cursor to the end on exit", async () => {
+    const fixture = "empty-elm-tooling";
+    const output = await toolsHelper(fixture, ["q"]);
+
+    expect(output).toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/empty-elm-tooling/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.2.8⧘
 
       ⧙Up⧘/⧙Down⧘ to move
       ⧙Space⧘ to toggle
@@ -55,29 +114,7 @@ describe("tools", () => {
       Nothing changed.
       ▊
     `);
-  });
 
-  test("cursor", async () => {
-    expect(await toolsHelper(["tools"], ["test-exit"])).toMatchInlineSnapshot(`
-      ⧙/Users/lydell/src/elm-tooling.json/cli/elm-tooling.json⧘
-
-      ⧙elm⧘
-        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
-        ⧙[⧘⧙☒⧘⧙]⧘ 0.19.1
-
-      ⧙elm-format⧘
-        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
-        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
-        ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
-        ⧙[⧘⧙x⧘⧙]⧘ 0.8.4
-
-      ⧙elm-json⧘
-        ⧙[⧘⧙x⧘⧙]⧘ 0.2.8
-
-      ⧙Up⧘/⧙Down⧘ to move
-      ⧙Space⧘ to toggle
-      ⧙Enter⧘ to save
-         
-    `);
+    expect(await toolsHelper(fixture, ["\x03"])).toBe(output);
   });
 });
