ae6fce118967671b4cdd77929fafb484dccd1e3c
Simon Lydell <simon.lydell@gmail.com>
2020-10-08 19:21:45 +0200

    Also test elm-tooling.json contents

--- cli/tests/tools.test.ts
+++ cli/tests/tools.test.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import * as path from "path";
 
 import elmToolingCli from "../index";
@@ -14,8 +15,9 @@ const FIXTURES_DIR = path.join(__dirname, "fixtures", "tools");
 async function toolsHelper(
   fixture: string,
   chars: Array<string>
-): Promise<string> {
+): Promise<{ stdout: string; json: string }> {
   const dir = path.join(FIXTURES_DIR, fixture);
+  const elmToolingJsonPath = path.join(dir, "elm-tooling.json");
 
   const stdout = new CursorWriteStream();
   const stderr = new MemoryWriteStream();
@@ -31,15 +33,21 @@ async function toolsHelper(
   expect(stderr.content).toBe("");
   expect(exitCode).toBe(0);
 
-  return clean(stdout.getOutput());
+  return {
+    stdout: clean(stdout.getOutput()),
+    json: fs.readFileSync(elmToolingJsonPath, "utf8"),
+  };
 }
 
 expect.addSnapshotSerializer(stringSnapshotSerializer);
 
 describe("tools", () => {
   test("default cursor position when no tools", async () => {
-    expect(await toolsHelper("empty-elm-tooling", ["test-exit"]))
-      .toMatchInlineSnapshot(`
+    const { stdout, json } = await toolsHelper("empty-elm-tooling", [
+      "test-exit",
+    ]);
+
+    expect(stdout).toMatchInlineSnapshot(`
       ⧙/Users/you/project/fixtures/tools/empty-elm-tooling/elm-tooling.json⧘
 
       ⧙elm⧘
@@ -60,11 +68,19 @@ describe("tools", () => {
       ⧙Enter⧘ to save
          
     `);
+
+    expect(json).toMatchInlineSnapshot(`
+      {}
+
+    `);
   });
 
   test("default cursor position when tools are provided", async () => {
-    expect(await toolsHelper("some-elm-tooling", ["test-exit"]))
-      .toMatchInlineSnapshot(`
+    const { stdout, json } = await toolsHelper("some-elm-tooling", [
+      "test-exit",
+    ]);
+
+    expect(stdout).toMatchInlineSnapshot(`
       ⧙/Users/you/project/fixtures/tools/some-elm-tooling/elm-tooling.json⧘
 
       ⧙elm⧘
@@ -85,13 +101,23 @@ describe("tools", () => {
       ⧙Enter⧘ to save
          
     `);
+
+    expect(json).toMatchInlineSnapshot(`
+      {
+        "tools": {
+          "elm-json": "0.2.8",
+          "elm-format": "0.8.3"
+        }
+      }
+
+    `);
   });
 
   test("moves cursor to the end on exit", async () => {
     const fixture = "empty-elm-tooling";
-    const output = await toolsHelper(fixture, ["q"]);
+    const { stdout, json } = await toolsHelper(fixture, ["q"]);
 
-    expect(output).toMatchInlineSnapshot(`
+    expect(stdout).toMatchInlineSnapshot(`
       ⧙/Users/you/project/fixtures/tools/empty-elm-tooling/elm-tooling.json⧘
 
       ⧙elm⧘
@@ -115,6 +141,11 @@ describe("tools", () => {
       ▊
     `);
 
-    expect(await toolsHelper(fixture, ["\x03"])).toBe(output);
+    expect(json).toMatchInlineSnapshot(`
+      {}
+
+    `);
+
+    expect(await toolsHelper(fixture, ["\x03"])).toEqual({ stdout, json });
   });
 });
