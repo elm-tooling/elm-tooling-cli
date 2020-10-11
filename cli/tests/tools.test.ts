f059b9d50a7c4bca0c971d43dbae65f55efa82ea
Simon Lydell <simon.lydell@gmail.com>
2020-10-11 14:13:29 +0200

    More tests

--- cli/tests/tools.test.ts
+++ cli/tests/tools.test.ts
@@ -118,6 +118,72 @@ describe("tools", () => {
     `);
   });
 
+  test("move cursor up past the edge", async () => {
+    const { stdout, json } = await toolsHelper("empty-elm-tooling", [
+      ...Array.from({ length: 100 }, () => "k"),
+      "test-exit",
+    ]);
+
+    expect(stdout).toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/empty-elm-tooling/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘▊⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.2.8⧘
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+    `);
+
+    expect(json).toMatchInlineSnapshot(`
+      {}
+
+    `);
+  });
+
+  test("move cursor down past the edge", async () => {
+    const { stdout, json } = await toolsHelper("empty-elm-tooling", [
+      ...Array.from({ length: 100 }, () => "j"),
+      "test-exit",
+    ]);
+
+    expect(stdout).toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/empty-elm-tooling/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘▊⧙]⧘ ⧙0.2.8⧘
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+    `);
+
+    expect(json).toMatchInlineSnapshot(`
+      {}
+
+    `);
+  });
+
   test("moves cursor to the end on exit", async () => {
     const fixture = "empty-elm-tooling";
     const { stdout, json } = await toolsHelper(fixture, ["q"]);
@@ -154,6 +220,86 @@ describe("tools", () => {
     expect(await toolsHelper(fixture, ["\x03"])).toEqual({ stdout, json });
   });
 
+  test("moves cursor to the end on unchanged save", async () => {
+    const fixture = "some-elm-tooling";
+    const { stdout, json } = await toolsHelper(fixture, ["x", "x", "\r"]);
+
+    expect(stdout).toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/some-elm-tooling/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘⧙x⧘⧙]⧘ 0.8.3
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘⧙x⧘⧙]⧘ 0.2.8
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+
+      Nothing changed.
+      ▊
+    `);
+
+    // Should be unchanged.
+    expect(json).toMatchInlineSnapshot(`
+      {
+        "tools": {
+          "elm-json": "0.2.8",
+          "elm-format": "0.8.3"
+        }
+      }
+
+    `);
+  });
+
+  test("toggle then exit", async () => {
+    const fixture = "some-elm-tooling";
+    const { stdout, json } = await toolsHelper(fixture, ["x", "q"]);
+
+    expect(stdout).toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/some-elm-tooling/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘⧙x⧘⧙]⧘ 0.2.8
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+
+      Nothing changed.
+      ▊
+    `);
+
+    // Should be unchanged.
+    expect(json).toMatchInlineSnapshot(`
+      {
+        "tools": {
+          "elm-json": "0.2.8",
+          "elm-format": "0.8.3"
+        }
+      }
+
+    `);
+  });
+
   test("change elm version", async () => {
     const { stdout, json } = await toolsHelper("change-elm-version", [
       "k",
@@ -185,11 +331,51 @@ describe("tools", () => {
       ▊
     `);
 
+    // Other fields are preserved and the order is unchanged.
     expect(json).toMatchInlineSnapshot(`
       {
+          "before": 1,
           "tools": {
               "elm": "0.19.0"
-          }
+          },
+          "after": 2
+      }
+
+    `);
+  });
+
+  test("removing last tool removes the entire field", async () => {
+    const { stdout, json } = await toolsHelper("remove-last-tool", ["o", "\r"]);
+
+    expect(stdout).toMatchInlineSnapshot(`
+      ⧙/Users/you/project/fixtures/tools/remove-last-tool/elm-tooling.json⧘
+
+      ⧙elm⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.0⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.19.1⧘
+
+      ⧙elm-format⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.1⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.2⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.3⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.8.4⧘
+
+      ⧙elm-json⧘
+        ⧙[⧘ ⧙]⧘ ⧙0.2.8⧘
+
+      ⧙Up⧘/⧙Down⧘ to move
+      ⧙Space⧘ to toggle
+      ⧙Enter⧘ to save
+
+      Saved!
+      ▊
+    `);
+
+    expect(json).toMatchInlineSnapshot(`
+      {
+          "entrypoints": [
+              "./src/Main.elm"
+          ]
       }
 
     `);
