849af20180372b7c0f10cbe6acd6fe421b64784a
Simon Lydell <simon.lydell@gmail.com>
2020-11-17 12:39:29 +0100

    Revert "Temp debug Windows"
    This reverts commit 798078e90a7e2400bd963097359cf0621a1a8775.

--- cli/tests/install.test.ts
+++ cli/tests/install.test.ts
@@ -265,7 +265,7 @@ describe("install", () => {
 
     if (IS_WINDOWS) {
       // eslint-disable-next-line jest/no-conditional-expect
-      expect(JSON.stringify(bin)).toMatchInlineSnapshot(`
+      expect(bin).toMatchInlineSnapshot(`
         elm
           something else
         elmx
@@ -297,7 +297,7 @@ describe("install", () => {
 
     if (IS_WINDOWS) {
       // eslint-disable-next-line jest/no-conditional-expect
-      expect(JSON.stringify(bin1)).toMatchInlineSnapshot(`
+      expect(bin1).toMatchInlineSnapshot(`
         elm
           #!/bin/sh
           '/Users/you/project/fixtures/install/create-links/elm-tooling/elm/0.19.1/elm' "$@"
@@ -387,7 +387,7 @@ describe("install", () => {
 
     `);
 
-    expect(IS_WINDOWS ? JSON.stringify(bin4) : bin4).toMatchInlineSnapshot(`
+    expect(bin4).toMatchInlineSnapshot(`
       elmx
         not elm
         
