f26bf7a8fb6823a6f0278cd0b51e30d85dcc0909
Simon Lydell <simon.lydell@gmail.com>
2023-09-09 13:37:16 +0200

    Fix TypeScript .d.ts files
    `export default` is apparently wrong for what we’re doing. `export =` is
    the correct one. Source: https://github.com/arethetypeswrong/arethetypeswrong.github.io/blob/4706e9a84892be17888bb01a4d85c0bfed3230c2/docs/problems/FalseExportDefault.md
    
    I’ve tested this with both `require` and `import` in both TS and JS
    files, with different `"module"` and `"moduleResolution"` settings, and
    it seems to work perfectly.
    
    With this commit, it is now possible to do:
    
        const elmToolingCli = require("elm-tooling")
    
    in an `@ts-check`ed JS file without errors – no errors at compile time, and no
    errors at runtime.

--- src/getExecutable.d.ts
+++ src/getExecutable.d.ts
@@ -1,7 +1,9 @@
-export default function getExecutable(options: {
+declare function getExecutable(options: {
   name: string;
   version: string;
   cwd?: string;
   env?: Record<string, string | undefined>;
   onProgress: (percentage: number) => void;
 }): Promise<string>;
+
+export = getExecutable;
